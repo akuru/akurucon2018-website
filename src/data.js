c5637b36055f062d2924c4c9d3c9393c5d3df205
Pubudu Kodikara <pubudu@leafycode.com>
2018-07-21 19:40:15 +0530

    Add schedule

---
+++ src/data.js
@@ -0,0 +1,136 @@
+export const schedules = [
+  {
+    day: 31,
+    month: 'AUG',
+    dayOfWeek: 'Friday',
+    events: [
+      {
+        time: '5:15 PM',
+        type: 'TALK',
+        title: 'My type design philosophy',
+        speaker: 'Martin Majoor',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '5:45 PM',
+        type: 'TALK',
+        title: 'My type design philosophy',
+        speaker: 'Martin Majoor',
+        venue: 'Mahaweli Centre'
+      }
+    ]
+  },
+  {
+    day: '01',
+    month: 'SEP',
+    dayOfWeek: 'Saturday',
+    events: [
+      {
+        time: '8:00 AM',
+        specialEventTitle: 'REGISTRATION AND TEA',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '9:00 AM',
+        type: 'WORKSHOP I',
+        title: 'Web typography',
+        speaker: 'Martin Majoor',
+        venue: 'RCU Skills Centre'
+      },
+      {
+        type: 'WORKSHOP II',
+        title: 'Experiencing Calligraphy',
+        speaker: 'Mahinda Bandara',
+        venue: 'RCU Skills Centre'
+      },
+      {
+        type: 'WORKSHOP III',
+        title: 'Font making tools',
+        speaker: 'Tharique Azeez',
+        venue: 'RCU Skills Centre'
+      },
+      {
+        type: 'WORKSHOP IV',
+        title: 'Palm leaf manuscripts',
+        speaker: 'Kelaniya University',
+        venue: 'RCU Skills Centre'
+      },
+      {
+        type: 'WORKSHOP V',
+        title: 'Play with Display',
+        speaker: 'Dr Sumanthri Samarawickrama',
+        venue: 'RCU Skills Centre'
+      },
+
+      {
+        time: '12:30 PM',
+        specialEventTitle: 'LUNCH BREAK',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '2:00 PM',
+        specialEventTitle: 'EVENT STARTS',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '2:15 PM',
+        type: 'INTRODUCTION',
+        title: 'Dutch-Sri Lankan Shared Typography Heritage Project',
+        speaker: 'Dutch Embassy',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '2:45 PM',
+        type: 'TALK',
+        title: 'The Dutch Press in Ceylon and its typography',
+        speaker: 'Dr Sumantri Samarawickrama',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '3:20 PM',
+        type: 'TALK',
+        title: 'History of sans-serifs',
+        speaker: 'Martin Majoor',
+        venue: 'Mahaweli Centre'
+      },
+
+      {
+        time: '4:00 PM',
+        specialEventTitle: 'TEA BREAK',
+        venue: 'Mahaweli Centre'
+      },
+
+      {
+        time: '4:15 PM',
+        type: 'TALK',
+        title: 'Coins and currency from the  Dutch Ceylon',
+        speaker: 'Dr Kavan Ratnatunga',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '4:45 PM',
+        type: 'TALK',
+        title: 'The last Dutch telephone book',
+        speaker: 'Martin Majoor',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '5:15 PM',
+        type: 'Galle fonts launch',
+        speaker: 'Mooniak',
+        venue: 'Mahaweli Centre'
+      },
+
+      {
+        time: '5:30 PM',
+        specialEventTitle: 'CLOSING REMARKS',
+        venue: 'Mahaweli Centre'
+      },
+      {
+        time: '7:30 PM',
+        specialEventTitle: 'PARTY',
+        venue: 'Mahaweli Centre'
+      }
+    ]
+  }
+];
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,8 +1,113 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { schedules } from '../data';
+
+const PageTitle = styled.h2`
+  font-family: Noto Sans ExtraCondensed;
+  font-weight: bold;
+  font-style: italic;
+  font-size: 1.9rem;
+  margin-top: 53px;
+  border-bottom: 3px solid #351544;
+  padding-bottom: 15px;
+`;
+
+const Event = styled.div`
+  display: grid;
+  grid-template-columns: 95px 0.7fr 1fr 0.8fr;
+  font-family: Noto Sans Condensed;
+  grid-column-gap: 20px;
+  margin-bottom: 45px;
+
+  &.new-event-group:not(:first-of-type) {
+    margin-top: 111px;
+  }
+`;
+
+const EventDate = styled.div`
+  .event-day-container {
+    display: grid;
+    grid-template-columns: 40px 55px;
+  }
+
+  .event-day {
+    grid-row: span 2;
+    font-weight: bold;
+    font-size: 2.2rem;
+    grid-row: span 2;
+    font-weight: bold;
+    font-size: 38px;
+    display: flex;
+    height: 42px;
+    margin-top: -8px;
+  }
+
+  .event-month {
+    font-weight: bold;
+  }
+
+  .event-day-of-week {
+    margin-top: -5px;
+  }
+`;
+
+const EventTime = styled.div`
+  font-weight: bold;
+`;
+
+const EventInfo = styled.div``;
+
+const SpecialEventInfo = styled.span`
+  font-weight: bold;
+  color: #eb682e;
+`;
+
+const EventVenue = styled.div`
+  text-align: right;
+`;
+
+const renderEvent = (schedule, event, index) => (
+  <Event className={index === 0 ? 'new-event-group' : ''}>
+    <EventDate>
+      {index === 0 && (
+        <div className="event-day-container">
+          <div className="event-day">{schedule.day}</div>
+          <div className="event-month">{schedule.month}</div>
+          <div className="event-day-of-week">{schedule.dayOfWeek}</div>
+        </div>
+      )}
+    </EventDate>
+    <EventTime>{event.time}</EventTime>
+    <EventInfo>
+      {event.specialEventTitle ? (
+        <SpecialEventInfo>{event.specialEventTitle}</SpecialEventInfo>
+      ) : (
+        <span>
+          <strong>
+            {event.type}
+            {event.title ? ':' : ''}
+          </strong>{' '}
+          {event.title}
+          <br /> by <strong>{event.speaker}</strong>
+        </span>
+      )}
+    </EventInfo>
+    <EventVenue>
+      <strong>@{event.venue}</strong>
+    </EventVenue>
+  </Event>
+);
 
 const Schedule = () => (
   <div>
-    <h2>Schedule</h2>
+    <PageTitle>Event Schedule</PageTitle>
+
+    <div>
+      {schedules.map(schedule =>
+        schedule.events.map((event, index) => renderEvent(schedule, event, index))
+      )}
+    </div>
   </div>
 );
 
